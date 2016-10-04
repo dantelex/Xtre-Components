6a5feb6bbbbcde17f28faa344c14bcf93dc98f9f
Karlista Okoh <dantelex2@gmail.com>
2016-10-04 17:31:01 +0100

    - Added more options for background images and img tags
    - Improved functionality

--- README.md
+++ README.md
@@ -1,6 +1,99 @@
 # Avatar-Initials
-Create initials with your name, instead of using images or use when no images.
+Create initials with your name, instead of using images. Also options to use images with image tag or set to background. You don't need any other html content. Takes care of using images and using initials
 
-It's a fun project i created. All you have to do is **add your name to the input box and sumbit**. It takes one or two initials only which is ideal. e.g *"Dante" would be "D" and "Dante Lex" would be "DL"*.
-You can improve it and use in your project.
+It takes one or two names. e.g *"Dante" would be "D" and "Dante Lex" would be "DL"*. It fits into the height and width of your container, modifiy style to suit your needs.
+You can improve and use in your project.
 
+## Include in your page
+###### Dependency
+```html
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+```
+###### Component - Unminified
+```html
+<link rel="stylesheet" type="text/css" href="css/placeholderavatar.css" />
+<script src="app.init.js"></script>
+<script src="app.placeholderavatar.js"></script>
+```
+###### Component - Minified
+```html
+<link rel="stylesheet" type="text/css" href="css/placeholderavatar.css" />
+<script src="app.min.js"></script>
+```
+
+### Initialize
+
+```javascript
+$(function(){
+	app.init({});
+});
+```
+
+### Usage
+    
+<p>Add: <code>data-xt-placeholder-avatar='name goes here'</code></p>
+
+```html
+<div data-xt-placeholder-avatar="Lex">
+</div>
+```
+
+![Image of inital](/img/1.png)
+
+<p>Change text size: <code>data-xt-avatar-size='medium, large or small'</code></p>
+
+```html
+<div data-xt-placeholder-avatar="Dante Lex" data-xt-avatar-size="medium">
+</div>
+```
+
+![Image of inital](/img/2.png)
+
+<p>Use background image: <code>data-xt-background-uimage='path to image'</code></p>
+
+```html
+<div data-xt-placeholder-avatar data-xt-background-uimage="img/profile.jpg">
+</div>
+```
+
+![Image of inital](/img/3.png)
+
+<p>Default state with no name: <code>data-xt-placeholder-avatar</code></p>
+
+```html
+<div data-xt-placeholder-avatar>
+</div>
+```
+
+![Image of inital](/img/4.png)
+
+<p>Use image: <code>data-xt-uimage='path to image'</code></p>
+
+```html
+<div data-xt-placeholder-avatar data-xt-uimage="img/profile.jpg">
+</div>
+```
+
+![Image of inital](/img/3.png)
+
+<div class="profile js-profile" data-xt-placeholder-avatar="" data-xt-uimage="img/profile.jpg">
+</div>
+
+<h4>Options</h4>
+<p>If you want colors to be handled from stylesheet</p>
+
+```javascript
+$(function(){
+	app.init({
+		placeholderAvatar: {
+			useColorClasses: true
+		}
+	});
+});
+```
+You can check it out here - 
+[avata-initials](http://github.com)
+
+<footer class="profile-footer">
+	<p>Developed By: <a href="https://twitter.com/Dantelex">@dantelex</a></p>
+</footer>
---
+++ js/app.placeholderavatar.js
@@ -0,0 +1,194 @@
+/**
+	Placeholder Avatar - Used for generating user initials and setting user image if neccessary
+
+	Usage: 'How to use component'
+
+	Element => 'data-vc-placeholder-avatar'
+
+	Example:
+
+*/
+
+;(function($, window, document, undefined){
+	window.app.comps.placeholderAvatar = {
+		config: {
+			placeholderAvatar: {
+				useColorClasses: false
+			}
+		},
+		init:function(){
+			// Contains the initialization code
+			var self = this,
+					cf = this.config,
+					opts = cf.placeholderAvatar;
+
+
+					opts.$container = $('[' + self.attr_name('placeholder-avatar') + ']');
+					opts.$avatarSize = $('[' + self.attr_name('avatar-size') + ']');
+					opts.$userImage = $('[' + self.attr_name('uimage') + ']');
+					opts.$backgroundImage = $('[' + self.attr_name('background-uimage') + ']');
+					opts.intitalsWrapper = '.js-user-initials-wrapper';
+					opts.intitals = '.js-user-initials';
+
+					$(opts.intitalsWrapper).remove();
+
+
+					if(!opts.$container.length)return false;
+
+					opts.$container.each(function(index, elem){
+						var $el = $(elem), //The current element
+						numColors = 24,
+						color;
+
+						// Colors set
+						if(opts.useColorClasses === false){
+							colors = ["#FF2800", "#ff3e1a", "#00A0B2", "#00bed3", "#FFF500", "#fff61a", "#24A597", "#2abeae", "#DE0052", "#fb005d", "#7B07A9", "#9208c9", "#ff6d00", "#ff7c1a", "#9c0", "#b0eb00", "#00BF32", "#00df3a", "orange", "#ffae1a", "#FF4637", "#ff594b", "#4212AF", "#4d15cc"];
+						}
+						else {
+						colors = 'color-0 color-1 color-2 color-3 color-4 color-5 color-6 color-7 color-8 color-9 color-10 color-11 color-12 color-13 color-14 color-15 color-16 color-17 color-18 color-19 color-20 color-21 color-22 color-23';
+						}
+
+						placeholderAvatar = $el.attr(self.attr_name('placeholder-avatar')),
+						userImage = $el.attr(self.attr_name('uimage')),
+						backgroundImage = $el.attr(self.attr_name('background-uimage')),
+						avatarSize = $el.attr(self.attr_name('avatar-size'));
+
+						var initials = placeholderAvatar && self.getInitials(placeholderAvatar); // Get the initials
+
+						// If the initials contains only numbers, then display no text
+						if(!isNaN(initials))initials = '';
+
+						var coloursIndex = Math.abs(self.hashCode(placeholderAvatar)) % numColors, //Get a random color from the color set
+						initialsContent = '<div class="user-initials js-user-initials"></div>',
+						$uInitialsContent;
+						if(opts.useColorClasses === false){
+							$uInitialsContent = $('<div class="user-initials-wrapper js-user-initials-wrapper">').html(initialsContent); //Create the html
+						}
+						else {
+							$uInitialsContent = $('<div class="user-initials-wrapper js-user-initials-wrapper color-0">').html(initialsContent); //Create the html
+						}
+
+						if(placeholderAvatar) {
+							$el.prepend($uInitialsContent); 
+							if(opts.useColorClasses === false){
+								$el.find(opts.intitalsWrapper).css('background-color', colors[coloursIndex]); //Change the background-color
+							}
+							else {
+							$el.find(opts.intitalsWrapper).removeClass(colors).addClass('color-' + coloursIndex); //Change the background-color
+							}
+							$el.find(opts.intitals).text(initials); //Show the initials
+						}
+
+						else if (placeholderAvatar === '' && (userImage === '' || backgroundImage === '')) {
+							$el.prepend($uInitialsContent); //Add the html content
+							if(opts.useColorClasses === false){
+								$el.find(opts.intitalsWrapper).css('background-color', "#f2f6f7"); //Change the background-color
+							}
+							else {
+								$el.find(opts.intitalsWrapper).removeClass(colors).addClass('color-g'); //Change the background-color
+							}
+							$el.find(opts.intitals).addClass('icon-user'); //Show the initials
+						}
+
+						else if (placeholderAvatar === '' && userImage) {
+							$el.prepend($uInitialsContent); //Add the html content
+							$el.find(opts.intitalsWrapper).removeClass(colors).addClass('pb-0').html(self.createImage(userImage)); //Add img tag with image
+						}
+
+						else if (placeholderAvatar === '' && backgroundImage) {
+							$el.prepend($uInitialsContent); //Add the html content
+							$el.find(opts.intitalsWrapper).removeClass(colors).addClass('pb-0, bimage').css('background-image', 'url(' + backgroundImage + ')'); //Add background image
+						}
+
+						else {
+							$el.prepend($uInitialsContent); //Add the html content
+							if(opts.useColorClasses === false){
+								$el.find(opts.intitalsWrapper).css('background-color', "#f2f6f7"); 
+							}
+							else {
+								$el.find(opts.intitalsWrapper).removeClass(colors).addClass('color-g'); //Change the background-color
+							}
+							
+							$el.find(opts.intitals).addClass('icon-user'); //Show the initials
+						}
+
+						//Changes the font-size of the initials
+						switch (avatarSize) {
+							case "small":
+									$el.find(opts.intitalsWrapper).addClass('avatar-small');
+								break;
+							case "medium":
+									$el.find(opts.intitalsWrapper).addClass('avatar-medium');
+								break;
+							case "large":
+									$el.find(opts.intitalsWrapper).addClass('avatar-large');
+								break;
+
+						}
+
+
+					});
+
+
+
+			this.events();
+
+		},
+
+		events:function(){
+			// Contains the event bindings and subscriptions
+			var self = this,
+					cf = this.config,
+					opts = cf.placeholderAvatar;
+
+
+		},
+
+		hashCode:function(msg) {
+			var self = this,
+					cf = this.config,
+					opts = cf.placeholderAvatar;
+
+					var hash = 0,
+								i, chr, len;
+					if (msg.length === 0) return hash;
+					for (i = 0, len = msg.length; i < len; i++) {
+								chr = msg.charCodeAt(i);
+								hash = ((hash << 5) - hash) + chr;
+								hash |= 0; // Convert to 32bit integer
+					}
+					return hash;
+		},
+
+		getInitials:function(msg, content) {
+			var self = this,
+					cf = this.config,
+					opts = cf.placeholderAvatar;
+
+
+					if (typeof content == "undefined") {
+								content = true;
+					}
+					var initials = msg.replace(/[^0-9a-zA-Z- ]/g, "").match(/\b\w/g);
+					if (content) {
+								return initials.join('').substring(0, 2);
+					}
+
+					return initials.substring(0, 2);
+
+		},
+
+		createImage:function(image) {
+			var self = this,
+					cf = this.config,
+					opts = cf.placeholderAvatar;
+
+        var thumb = '<img src="' + image + '">';
+
+        return thumb;
+    },
+
+
+
+	};
+}(jQuery, window, document));
